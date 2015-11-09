534df80d70636668739f050348ad6a33b0a31381
adsmith1217 <adsmith1217@gmail.com>
2015-11-08 19:28:34 -0500

    Rand pass gen, root pass block, noti alerts

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,6 +16,8 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+// Cordova Standard Included Init Functions
 var app = {
     // Application Constructor
     initialize: function() {
@@ -48,4 +50,148 @@ var app = {
     }
 };
 
+// App initialization, custom JS
 app.initialize();
+
+// Create master password
+var onepass = "";
+
+// Store master password
+function store_onepass()
+{
+    onepass = document.getElementById("master_password").value;
+
+    //Prompt if user enters blank input
+    if(onepass == "")
+    {
+        navigator.notification.alert("Please enter your 1Pass.");
+        return;
+    }
+    if(onepass.length >= 20)
+    {
+        alert("Your password must be 20 characters or less."); 
+        return; 
+    }
+
+    // Hash given password
+    var onepass_hash = CryptoJS.MD5(onepass);
+
+    // New user who has never logged in before, so we need to add their password
+    if(localStorage.getItem("hashed1pass") === null)
+    {
+        localStorage.setItem('hashed1pass', onepass_hash); 
+        $.mobile.changePage($("#pagetwo"), "slide", true, true); // TODO: create a fifth page for hint management and direct to there
+    }
+    // User has logged in before, so we need to verify their password
+    else {
+        if(localStorage.getItem('hashed1pass') != CryptoJS.MD5(onepass))
+        {
+            navigator.notification.alert("The password you have entered is incorrect."); 
+            return; 
+        }
+        display_list(); 
+        $.mobile.changePage($("#pagefour"), "slide", true, true);
+    }
+}
+
+//Adds a new account and encrypts password
+function new_account() {
+
+    // New account details
+    var name = document.getElementById("new_name").value;
+    var password = document.getElementById("new_password").value;
+
+    // Prompt if user enters blank input
+    if(name == "" || password == "")
+    {
+        navigator.notification.alert("Please enter a site name and password.");
+        return;
+    }
+    if(CryptoJS.MD5(password) == localStorage.getItem('hashed1pass'))
+    {
+        navigator.notification.alert("Please use a password besides your 1Pass.");
+        return;
+    }
+
+    // TODO: Add in if statement to prevent weak passwords
+
+    // Encryption using CryptoJS
+    var options = {
+        mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7
+    };
+    var encrypted_password = CryptoJS.AES.encrypt(password, onepass, options);
+    // Add account name and encrypted password to localStorage object
+    localStorage.setItem(name, encrypted_password)
+    build_list();
+    $.mobile.changePage($("#pagefour"), "slide", true, true);
+}
+
+// TODO: Add a function to remove a password from the list
+function remove_account() {
+
+}
+
+// Helper function for "Show Password List" button. Build list and navigate to page 4 (account list)
+function display_list()
+{
+    build_list();
+    $.mobile.changePage($("#pagefour"), "slide", true, true);
+}
+
+// Build account list by iterating through localStorage object
+function build_list()
+{
+    // Create a list object
+    var list = "";
+
+    // 
+    for(var key in localStorage) // TODO: block onepass as a key
+    {
+        list = list + "<li><a href='javascript:decrypt_item(\"" + key + "\")'>" + key + "</a></li>";
+    }
+    //Updates the list of accounts with the new list
+    document.getElementById("ul_list").innerHTML = list;
+}
+
+// Decrypts stored account password from given account key
+function decrypt_item(name)
+{      
+    // Decryption using CryptoJS
+    var options = { mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 };
+    var decrypted_password = CryptoJS.AES.decrypt(localStorage.getItem(name), onepass, options);
+
+    // Notification displays decrypted password
+    console.log(decrypted_password.toString(CryptoJS.enc.Utf8));
+    navigator.notification.alert("Password is: " + decrypted_password.toString(CryptoJS.enc.Utf8));
+}
+
+//Random Password Generator
+function generatePassword() {
+    var length = 8;
+        charset = "abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    navigator.notification.alert("Your random password is: " + retVal)
+}
+
+// TODO: Function for changing the hint
+
+// Refreshes to list of accounts everytime page is visited
+$(document).on("pageshow","#pagefour",function()
+{
+  $("#ul_list").listview("refresh");
+});
+
+
+
+
+
+
+
+
+
+
+
+
--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,27 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.2-dev",
+    "cordova-plugin-vibration": "2.0.0-dev"
 }
 // BOTTOM OF METADATA
 });
